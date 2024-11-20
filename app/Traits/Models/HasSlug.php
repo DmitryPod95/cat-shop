93d0e010d73b4cfffcbc48ed79ee6092810324c9
Дмитрий Подлипский <Poddim95@yandex.ru>
2024-11-20 09:31:46 +0300

    - уникальный slug при создании

--- app/Traits/Models/HasSlug.php
+++ app/Traits/Models/HasSlug.php
@@ -16,8 +16,48 @@ trait HasSlug
         });
     }
 
-    protected static function slugFrom(): string
+    protected function makeSlug(): void
+    {
+        $slug = $this->slugUnique(
+            str($this->{$this->slugFrom()})
+                ->slug()
+                ->value()
+        );
+
+        $this->{$this->slugColumn()} = $this->{$this->slugColumn()} ?? $slug;
+    }
+
+    protected function slugColumn(): string
+    {
+        return 'slug';
+    }
+
+    protected function slugFrom(): string
     {
         return 'title';
+    }
+
+    private function slugUnique( string $slug): string
+    {
+        $originalSlug = $slug;
+        $i = 0;
+
+        while($this->isSlugExists($slug)) {
+            $i++;
+
+            $slug = $originalSlug . '-'. $i;
+
+        }
+
+        return $slug;
+    }
+
+    private function isSlugExists(string $slug): bool
+    {
+        $query = $this->newQuery()
+            ->where(self::slugColumn(), $slug)
+            ->withoutGlobalScopes();
+
+        return $query->exists();
     }
 }
